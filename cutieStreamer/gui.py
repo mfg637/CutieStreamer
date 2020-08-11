a296a543bb97bdf40635808a7008168def8de2cd
mfg637 <mfg636@gmail.com>
2020-08-11 16:27:22 +0300

    remove last.fm API support

--- cutieStreamer/gui.py
+++ cutieStreamer/gui.py
@@ -9,14 +9,7 @@ import threading
 from platform import system
 import pynput
 
-LAST_FM = None
-#try:
-#	import pylast
-#except ImportError:
-#	LAST_FM = False
-#else:
-#	LAST_FM = True
-#	from . import last_fm
+
 from tkinter import Tk, PhotoImage, Menu, DISABLED, Frame, Label, HORIZONTAL, NORMAL
 from tkinter import ttk, filedialog, messagebox
 from audiolib import tagIndexer
@@ -26,7 +19,7 @@ import gui
 import converter
 
 
-support_soundfiles_extensions = ("*.wav *.mp3 *.m4a *.flac *.ogg *.opus"
+support_sound_files_extensions = ("*.wav *.mp3 *.m4a *.flac *.ogg *.opus"
 								"*.ape *.tak *.tta *wv *mka")
 
 
@@ -62,9 +55,6 @@ class GUI:
 		self.playback_mode=0
 		self.plmd=0
 		self._trackPropertiesFrame=None
-		if LAST_FM:
-			self.last_fm = last_fm.Last_fm_Scrobbler(self)
-		self._last_fm_scrobbling_update_timer = None
 
 		self._inf_labels = gui.components.TrackInfo(self._root, width=320)
 		self._inf_labels.grid(row=0, column=0, sticky='w')
@@ -98,7 +88,6 @@ class GUI:
 
 		self._filemenu = Menu(self._menubar, tearoff=0)
 		self._filemenu.add_command(label="Show properties", command=self.__showPropertiesFrame)
-		#self._filemenu.add_command(label="Synchronise to mobile", command=self.__runAlbumSync)
 		self._filemenu.add_command(label="Exit", command=self._root.quit)
 		self._menubar.add_cascade(label="File", menu=self._filemenu)
 
@@ -133,16 +122,10 @@ class GUI:
 			command=self.__save_playlist, state=DISABLED)
 		self._menubar.add_cascade(label="Playlist", menu=self._playlist_menu)
 
-		if LAST_FM:
-			self._last_fm_menu = Menu(self._menubar, tearoff=0)
-			self._last_fm_menu.add_command(label="login", command=self.__last_fm_open_connection)
-			self._last_fm_menu.add_command(label="love this track", command=self.__lastfm_add_to_favs)
-			self._menubar.add_cascade(label="Last.fm", menu=self._last_fm_menu)
-
 		self._root.config(menu=self._menubar)
 
 		timeline_kwargs = {"orient": HORIZONTAL, "showvalue": 0, "sliderlength": 10}
-		if system()=="Windows":
+		if system() == "Windows":
 			timeline_kwargs["length"] = 310
 		else:
 			timeline_kwargs["length"] = 340
@@ -191,9 +174,6 @@ class GUI:
 		self._bitrade_label.pack(side="left")
 		self._playstate_label = Label(self._status_bar)
 		self._playstate_label.pack(side="left")
-		if LAST_FM:
-			self._lastfm_conection_label = Label(self._status_bar)
-			self._lastfm_conection_label.pack(side="left")
 		self._status_bar.grid(row=5, column=0, sticky='w')
 
 		if _playlist is not None and \
@@ -214,7 +194,6 @@ class GUI:
 
 		self._root.mainloop()
 
-
 	def _togle_play_state(self):
 		if self._playlist.isPlaying():
 			self._playlist.pause()
@@ -222,20 +201,12 @@ class GUI:
 			self._playpause_btn.setPlay()
 			self.__lock_prev_next_buttons()
 			self._root.after_cancel(self._playing_timer)
-			self.cancelScrobblingUpdateTimer()
 		else:
 			self._playlist.play()
 			self._playpause_btn.setPause()
 			self._playback_menu.entryconfig(1, state=NORMAL)
 			self._current_track_number = self._playlist.currentPosition()['track']
-			if LAST_FM and self.last_fm.isConnected():
-				lfm_thread=threading.Thread(
-					target=self.last_fm.now_playing,
-					args=(self._playlist.tags[self._current_track_number],)
-				)
-				lfm_thread.start()
 			self._playing_update()
-
 
 	def _stop(self, event=None):
 		self._playlist.stop()
@@ -243,28 +214,16 @@ class GUI:
 		self._playstate_label['text'] = 'playback stoped'
 		self.__lock_prev_next_buttons()
 		self._root.after_cancel(self._playing_timer)
-		self.cancelScrobblingUpdateTimer()
 		self._timeline.reset()
 		self._playback_menu.entryconfig(1, state=DISABLED)
-
 
 	def select_item(self, track):
 		if self._playing_timer is not None:
 			self._root.after_cancel(self._playing_timer)
 		self._playlist.PlayFromItem(track)
 		self._playback_menu.entryconfig(1, state=NORMAL)
-		if LAST_FM and self.last_fm.isConnected():
-			Position = self._playlist.currentPosition()
-			lfm_scroble_thread = threading.Thread(
-				target = self.last_fm.scrobble,
-				args = (
-					self._playlist.tags[Position['track']],
-				)
-			)
-			lfm_scroble_thread.start()
 		self._playing_update()
 		self._playpause_btn.setPause()
-
 
 	def _playing_update(self):
 		if not self._playlist.isEnd():
@@ -287,35 +246,16 @@ class GUI:
 			self._bitrade_label['text'] = str(
 				int(self._playlist.tags[Position['track']].bitrate()))+'kbps'
 			self._playstate_label['text'] = self._playlist.playback_mode+' playback'
-			if LAST_FM and self.last_fm.isConnected():
-				lfm_scroble_thread = threading.Thread(
-					target = self.last_fm.scrobble,
-					args = (
-						self._playlist.tags[Position['track']],
-					)
-				)
-				lfm_scroble_thread.start()
 		else:
 			self._playpause_btn.setReplay()
-			if LAST_FM and self.last_fm.isConnected():
-				lfm_scroble_thread = threading.Thread(
-					target = self.last_fm.scrobble,
-					args = (
-						None,
-					)
-				)
-				lfm_scroble_thread.start()
-
 
 	def _timeline_ready(self):
 		self._root.after_cancel(self._playing_timer)
-
 
 	def _timeline_seek(self, position: float):
 		self._playlist.seek(position)
 		self._playing_timer = self._root.after(100, self._playing_update)
 		self._playpause_btn.setPause()
-
 
 	def _playlist_creator(self):
 		self.display_loading_banner(True)
@@ -324,7 +264,7 @@ class GUI:
 		self._timeline.reset()
 		filelist = filedialog.askopenfilenames(parent=self.loading_banner.root,
 			title="select sound files",
-			filetypes=(("sound files", support_soundfiles_extensions),
+			filetypes=(("sound files", support_sound_files_extensions),
 			("index files", "*.cue"),
 			("Winamp playlist", "*.m3u *.m3u8"),
 			("all files", "*.*"))
@@ -360,13 +300,12 @@ class GUI:
 			self._playing_update()
 		self.hide_loading_banner()
 
-
 	def _add_track_in_playlist(self):
 		self.display_loading_banner(True)
 		self.freeze()
 		filelist = filedialog.askopenfilenames(parent=self.loading_banner.root,
 			title="select sound files",
-			filetypes=(("sound files", support_soundfiles_extensions),
+			filetypes=(("sound files", support_sound_files_extensions),
 				("index files", "*.cue"),
 				("all files", "*.*")))
 		if len(filelist):
@@ -389,7 +328,6 @@ class GUI:
 			self._playing_update()
 		self.hide_loading_banner()
 
-
 	def _addDirectoryToPlaylist(self):
 		self.display_loading_banner(True)
 		self.freeze()
@@ -411,7 +349,6 @@ class GUI:
 		elif self._playlist is not None and self._playlist.isPlaying():
 			self._playing_update()
 		self.hide_loading_banner()
-
 
 	def _scandir(self):
 		self.display_loading_banner(True)
@@ -446,7 +383,6 @@ class GUI:
 			self._playing_update()
 		self.hide_loading_banner()
 
-
 	def __prev_track_change(self):
 		Position = self._playlist.currentPosition()
 		if not self._playlist.isStart():
@@ -456,16 +392,7 @@ class GUI:
 			self._playlist.PlayFromItem(prev_track)
 			if prev_track == 0:
 				self._prev_btn.disable()
-		if LAST_FM and self.last_fm.isConnected():
-			lfm_scroble_thread = threading.Thread(
-				target = self.last_fm.scrobble,
-				args = (
-					self._playlist.tags[Position['track']],
-				)
-			)
-			lfm_scroble_thread.start()
 		self._playing_update()
-
 
 	def __next_track_change(self):
 		Position = self._playlist.currentPosition()
@@ -478,16 +405,7 @@ class GUI:
 				self._next_btn.enable()
 			else:
 				self._next_btn.disable()
-		if LAST_FM and self.last_fm.isConnected():
-			lfm_scroble_thread = threading.Thread(
-				target = self.last_fm.scrobble,
-				args = (
-					self._playlist.tags[Position['track']],
-				)
-			)
-			lfm_scroble_thread.start()
 		self._playing_update()
-
 
 	def __lock_playback_controls(self):
 		self._playpause_btn.disable()
@@ -496,7 +414,6 @@ class GUI:
 		self._prev_btn.disable()
 		self._next_btn.disable()
 		self._playback_menu.entryconfig(1, state=DISABLED)
-
 
 	def __unlock_playback_controls(self):
 		self._playpause_btn.enable()
@@ -508,7 +425,6 @@ class GUI:
 		self._playlist_menu.entryconfig(3, state=NORMAL)
 		self._playlist_menu.entryconfig(6, state=NORMAL)
 
-
 	def display_loading_banner(self, window_raise=False):
 		self.loading_banner = gui.dialogues.LoadingBanner(
 			self._root,
@@ -516,10 +432,8 @@ class GUI:
 			window_raise
 		)
 
-
 	def hide_loading_banner(self):
 		del self.loading_banner
-
 
 	def setGaplessMode(self):
 		if self._playing_timer is not None:
@@ -529,7 +443,6 @@ class GUI:
 			self._playing_update()
 		self.playback_mode=0
 
-
 	def setCrossfadeMode(self):
 		if self._playing_timer is not None:
 			self._root.after_cancel(self._playing_timer)
@@ -538,11 +451,9 @@ class GUI:
 			self._playing_update()
 		self.playback_mode=1
 
-
 	def __lock_prev_next_buttons(self):
 		self._prev_btn.disable()
 		self._next_btn.disable()
-
 
 	def quit(self):
 		if self._playlist is not None:
@@ -550,7 +461,6 @@ class GUI:
 			del self._playlist
 		self._keyboard_listener.stop()
 		self._root.destroy()
-
 
 	def __save_playlist(self):
 		if self._playing_timer is not None:
@@ -565,7 +475,6 @@ class GUI:
 				self._playlist.serialize(filename)
 		if self._playlist is not None and self._playlist.isPlaying():
 			self._playing_update()
-	
 
 	def setPlaylist(self, _playlist, cover_thumbnails):
 		if self._playing_timer is not None:
@@ -627,7 +536,6 @@ class GUI:
 			self._playing_update()
 		self.hide_loading_banner()
 
-
 	def __showPropertiesFrame(self):
 		if self._playlist.isPlaying():
 			Position=self._playlist.currentPosition()
@@ -636,7 +544,6 @@ class GUI:
 		else:
 			messagebox.showerror('DisplayProperties',
 									'Can`t display track properties: nothing is playing')
-
 
 	def clear(self):
 		self._playlist_box.clear()
@@ -662,34 +569,16 @@ class GUI:
 		self._bitrade_label['text'] = ''
 		self._playstate_label['text'] = ''
 
-
 	def playlist_getCoverThumbnails(self):
 		return self._playlist_box.getCoverThumbnails()
-
-
-	def __last_fm_open_connection(self):
-		if LAST_FM and not self.last_fm.isConnected():
-			self.last_fm.authorisation(self._root, self)
-	
-
-	def __lastfm_add_to_favs(self):
-		if LAST_FM and self.last_fm.isConnected():
-			cp = self._playlist.currentPosition()['track']
-			self.last_fm.loveThisTrack(self._playlist.tags[cp])
-
 
 	def freeze(self):
 		if self._playing_timer is not None:
 			self._root.after_cancel(self._playing_timer)
-	
 
 	def unfreeze(self):
 		if self._playlist is not None and self._playlist.isPlaying():
 			self._playing_update()
-	
-
-	def lastfm_connection_active(self):
-		self._lastfm_conection_label['text'] = 'Last.fm conection'
 
 	def openAlbumDialogue(self, album, artist, tracks):
 		self.freeze()
@@ -716,22 +605,6 @@ class GUI:
 			args = (tracks, gui.dialogues.ConvertProgress(self._root))
 		)
 		convert_thread.start()
-
-	def setScrobblingUpdateTimer(self):
-		self._last_fm_scrobbling_update_timer = self._root.after(30000, self.scrobblingUpdateEvent)
-
-	def cancelScrobblingUpdateTimer(self):
-		if self._last_fm_scrobbling_update_timer is not None:
-			self._root.after_cancel(self._last_fm_scrobbling_update_timer)
-	
-	def scrobblingUpdateEvent(self):
-		if LAST_FM and self.last_fm.isConnected():
-			current_track = self._playlist.currentPosition()['track']
-			lfm_thread=threading.Thread(
-				target=self.last_fm.now_playing,
-				args=(self._playlist.tags[current_track],)
-			)
-			lfm_thread.start()
 	
 	def __seek_back(self, event):
 		if self._playlist.isPlaying():
